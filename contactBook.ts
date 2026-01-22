941145849d9ec32255ae710b0c582d91b3d82201
Mina R <minarumang@icloud.com>
2026-01-22 21:27:33 +0100

    add remove by id

--- contactBook.ts
+++ contactBook.ts
@@ -9,7 +9,7 @@ interface Contact {
 let contacts: Contact[] = []; /*det här är en array(och den är tom)*/
 
 /*Step 4 function 1*/
-function addContact(contact: Contact): void {
+function addContact(contact: Contact): void { /*void betyder att function returns nothing*/
     contacts.push(contact);
     console.log(`Contact "${contact.name}" added successfully`);
 }
@@ -25,3 +25,23 @@ function listContacts(): void {
         );
     });
 }
+
+/*function 3*/
+function findByName(name: string): Contact[] {
+    const matches = contacts.filter(contact => contact.name === name);
+    console.log(`Found ${matches.length} contact(s) with name "${name}"`);
+    return matches;
+}
+/*function 4*/
+function removeById(id: number): boolean {
+    const index = contacts.findIndex(contact => contact.id === id);
+    if (index !== -1) { // -1 kör koden bara om kontakten finns i arrayen
+        const removed = contacts.splice(index, 1); //splice betyder att det tar bort elementet
+        console.log(`Contact "${removed[0]!.name}" with ID ${id} has been removed.`);
+        return true;
+    }
+    else {
+            console.log(`No contact found with ID ${id}`);
+            return false;
+        }
+    }
